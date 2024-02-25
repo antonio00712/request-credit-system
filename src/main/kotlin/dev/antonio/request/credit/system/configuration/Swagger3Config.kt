f70f330475f76456f0b5e1a9d98af0902818e7b1
Antonio <antonio00712@gmail.com>
2024-02-25 15:24:02 -0300

    add swaggerConfig and implementation of springdoc

---
+++ src/main/kotlin/dev/antonio/request/credit/system/configuration/Swagger3Config.kt
@@ -0,0 +1,14 @@
+package dev.antonio.request.credit.system.configuration
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Swagger3Config {
+    @Bean
+    fun publicApi(): GroupedOpenApi? {
+        return GroupedOpenApi.builder().group("springrequestcreditsystem-public")
+            .pathsToMatch("/api/customers/**", "/api/credits/**").build()
+    }
+}
