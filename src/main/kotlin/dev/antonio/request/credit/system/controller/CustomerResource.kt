4f113c0ee78474449c4a4a0a14c072f62dadbd81
Antonio <antonio00712@gmail.com>
2024-02-26 13:57:03 -0300

    add CustomerResourceTest.kt

--- src/main/kotlin/dev/antonio/request/credit/system/controller/CustomerResource.kt
+++ src/main/kotlin/dev/antonio/request/credit/system/controller/CustomerResource.kt
@@ -26,9 +26,9 @@ class CustomerResource (
 ) {
 
     @PostMapping
-    fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<String> {
+    fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<CustomerView> {
         val savedCustomer = this.customerService.save(customerDto.toEntity())
-        return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${savedCustomer.firstName} saved!")
+        return ResponseEntity.status(HttpStatus.CREATED).body(CustomerView(savedCustomer))
     }
 
     @GetMapping("/{id}")
